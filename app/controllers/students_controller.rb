e40a352c2abcb44b86defb054d833164ac5ffe50
Vivian Yim <keroviv86@gmail.com>
2022-02-21 22:48:39 -0600

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,4 +9,14 @@ class StudentsController < ApplicationController
     student = Student.find(params[:id])
     render json:student
   end
+
+  def index
+    students = if params[:name]
+                 Student.by_name(params[:name])
+               else
+                 Student.all
+               end    
+    render json: students
+  end
+
 end
